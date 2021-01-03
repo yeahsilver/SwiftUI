399a0e07f3dcaab0dbe4f1aaa085209252a3d443
YeeunHeo <gjdpdms2005@gmail.com>
2021-01-03 22:21:13 +0900

    ㅡ ㅣㅏㅡ ㅣㅏㅡ

---
+++ Chart/Chart/ChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartApp.swift
+//  Chart
+//
+//  Created by 허예은 on 2021/01/03.
+//
+
+import SwiftUI
+
+@main
+struct ChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chart/Chart/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Chart
+//
+//  Created by 허예은 on 2021/01/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LineCharts()
+    }
+}
+
+struct LineCharts:View {
+    var body: some View {
+        VStack{
+            LineChartView(data: [8,23,54,32,12,37,7,23,43], title: "Title")
+        }
+    }
+}
